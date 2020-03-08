9f76c5b2e5e2035052c4989dc77d3c93bc8717a0
Gao Tian Run <gaotianrun@iMac.local>
2020-03-08 22:46:38 +0800

    l

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ const navSlide = () => {
     });
 }
 
-navSlide();
+function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * speed}px)`;
+}
+
+const runAll = () => {
+    navSlide();
+    window.addEventListener("scroll", function(){
+        parallax(".intro", window.scrollY, 0.7);
+        parallax(".introtext", window.scrollY, -0.07);
+    });
+}
+
+runAll();
